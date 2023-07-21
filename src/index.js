e92b08b5ba3c6bc9697253d1a1bea70c1e30feee
unknown <kipkemoiedgah@gmail.com>
2023-07-21 10:50:21 +0300

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,40 +1,39 @@
 const tasks = [
-    {
-      description: 'Todays To Do',
-      completed: false,
-      index: 1,
-    },
-    {
-      description: 'Add to your list...',
-      completed: false,
-      index: 2,
-    },
-    {
-      description: 'wash the dishes',
-      completed: true,
-      index: 3,
-    },
-    {
-      description: 'complete To Do list project',
-      completed: false,
-      index: 4,
-    },
-    {
-      description: 'Clear all completed',
-      completed: false,
-      index: 5,
-    },
-  ];
-  
-  function renderTasks() {
-    const todoList = document.getElementById('todo-list');
-  
-    tasks.forEach((task) => {
-      const listItem = document.createElement('li');
-      listItem.textContent = task.description;
-      todoList.appendChild(listItem);
-    });
-  }
-  
-  document.addEventListener('DOMContentLoaded', renderTasks);
-  
+  {
+    description: 'Todays To Do',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Add to your list...',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'wash the dishes',
+    completed: true,
+    index: 3,
+  },
+  {
+    description: 'complete To Do list project',
+    completed: false,
+    index: 4,
+  },
+  {
+    description: 'Clear all completed',
+    completed: false,
+    index: 5,
+  },
+];
+
+function renderTasks() {
+  const todoList = document.getElementById('todo-list');
+
+  tasks.forEach((task) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = task.description;
+    todoList.appendChild(listItem);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', renderTasks);
